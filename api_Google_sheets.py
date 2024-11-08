6a1789a608172ba786a15ce718b203424ff32989
f0men <egorfomenko867@gmail.com>
2024-11-08 10:59:47 +0300

    change dir for keyfile

--- api_Google_sheets.py
+++ api_Google_sheets.py
@@ -15,7 +15,7 @@ def write_daily_info(data):
          'https://www.googleapis.com/auth/spreadsheets',
          'https://www.googleapis.com/auth/drive.file',
          'https://www.googleapis.com/auth/drive']
-    credentials = ServiceAccountCredentials.from_json_keyfile_name('.\dailyreportspythongs-fe6d3bc3bc0f.json', scope)
+    credentials = ServiceAccountCredentials.from_json_keyfile_name('./dailyreportspythongs-fe6d3bc3bc0f.json', scope)
     gc = gspread.authorize(credentials)
     sheet = gc.open("daily_activity").worksheet('Sheet1')
     sheet.update_acell("A1", 'Number of attempts')
