629457a5980e484df9397995855c2f46c85877cd
f0men <egorfomenko867@gmail.com>
2024-11-08 21:57:13 +0300

    add new function tg_bot

--- tg_bot.py
+++ tg_bot.py
@@ -1,23 +1,25 @@
 import os
-
 import telebot
 from telebot import types
 from dotenv import load_dotenv
 
-dirname = os.path.dirname(__file__) # сохрнение название директори текущего файла
-dotenv_path = os.path.join(dirname, '.env') # сохранение директории env-файла
-load_dotenv(dotenv_path) # загрузка данных env-файла
+# Начальная настройка
+dirname = os.path.dirname(__file__)  # сохранение названия директории текущего файла
+dotenv_path = os.path.join(dirname, '.env')  # сохранение директории env-файла
+load_dotenv(dotenv_path)  # загрузка данных env-файла
 
 TOKEN = os.getenv('BOT_TOKEN')
 
 bot = telebot.TeleBot(TOKEN)
-
 
 user_state = {}  # Глобальная переменная для отслеживания состояния пользователя
 
 @bot.message_handler(commands=['start'])
 def start(message):
     user_state[message.chat.id] = 'main_menu'  # Установка состояния пользователю
+    send_main_menu(message)  # Отправка кнопок сразу
+
+def send_main_menu(message):
     markup = types.ReplyKeyboardMarkup()
     button_1 = types.KeyboardButton('Перейти в Google sheets')
     button_2 = types.KeyboardButton('Отобразить графики в чате')
@@ -28,34 +30,47 @@ def start(message):
     markup.row(button_3)
     markup.row(button_4)  # Добавление кнопки возврата в меню
     bot.send_message(message.chat.id, 'Привет! Выбери следующее действие', reply_markup=markup)
-    bot.register_next_step_handler(message, click)
 
+def send_markup(message):
+    markup = types.ReplyKeyboardMarkup()
+    button_1 = types.KeyboardButton('Перейти в Google sheets')
+    button_2 = types.KeyboardButton('Отобразить графики в чате')
+    button_3 = types.KeyboardButton('Получить данные в формате txt')
+    button_4 = types.KeyboardButton('Перезапуск')  # Кнопка для возврата
+    markup.row(button_1)
+    markup.row(button_2)
+    markup.row(button_3)
+    markup.row(button_4)  # Добавление кнопки возврата в меню
+    bot.send_message(message.chat.id,'Неизвестная команда. Пожалуйста, выберите действие из меню.', reply_markup=markup)
+
+@bot.message_handler(func=lambda message: True)
 def click(message):
     # Сохранение состояния
     state = user_state.get(message.chat.id, 'main_menu')
-    
+
     if state != 'main_menu':
-        return start(message)  # Возвращаем в меню, если состояние не соответствует
-    
+        return send_main_menu(message)  # Возвращаем в меню, если состояние не соответствует
+
     if message.text == 'Отобразить графики в чате':
         last_file = os.listdir(f'{dirname}/DataTelegramBot/images')[0]
         file = open(f'{dirname}/DataTelegramBot/images/' + last_file, 'rb')
         bot.send_photo(message.chat.id, file)
-        bot.register_next_step_handler(message, click)
-
+        
     elif message.text == 'Перейти в Google sheets':
         markup_for_button_link = types.InlineKeyboardMarkup()
         markup_for_button_link.add(types.InlineKeyboardButton('GS', url='https://docs.google.com/spreadsheets/d/1h8RrekZGeL4NfZ8_6sJpaW_RSv7NS3KDhaP5k3RWlT8/edit?usp=sharing'))
         bot.send_message(message.chat.id, 'Ссылка:', reply_markup=markup_for_button_link)
-        bot.register_next_step_handler(message, click)
-
+        
     elif message.text == 'Получить данные в формате txt':
         last_file = os.listdir(f'{dirname}/DataTelegramBot/reports')[0]
         file = open(f'{dirname}/DataTelegramBot/reports/' + last_file, 'rb')
         bot.send_document(message.chat.id, file)
-        bot.register_next_step_handler(message, click)
-
+        
     elif message.text == 'Перезапуск':  # Если нажата кнопка возврата
-        start(message)  # Вызов функции start для возврата в главное меню
+        send_main_menu(message)  # Просто отправляем меню
+
+    else:  # Если команда не распознана
+        # Сообщение о неизвестной команде
+        send_markup(message) 
 
 bot.polling(none_stop=True)
