b68b340a6227615f93c6d7ee3ddb5365c120befc
f0men <egorfomenko867@gmail.com>
2024-11-12 13:18:14 +0300

    change dir in api_Google_sheets file

--- api_Google_sheets.py
+++ api_Google_sheets.py
@@ -10,12 +10,14 @@ from oauth2client.service_account import ServiceAccountCredentials
 
 yesterday_date = datetime.datetime.strftime(date.today()-timedelta(days=1),"%Y-%m-%d")
 
+dirname = os.path.dirname(__file__)
+
 def write_daily_info(data):
     scope = ['https://spreadsheets.google.com/feeds',
          'https://www.googleapis.com/auth/spreadsheets',
          'https://www.googleapis.com/auth/drive.file',
          'https://www.googleapis.com/auth/drive']
-    credentials = ServiceAccountCredentials.from_json_keyfile_name('./dailyreportspythongs-fe6d3bc3bc0f.json', scope)
+    credentials = ServiceAccountCredentials.from_json_keyfile_name(f'{dirname}/dailyreportspythongs-fe6d3bc3bc0f.json', scope)
     gc = gspread.authorize(credentials)
     sheet = gc.open("daily_activity").worksheet('Sheet1')
     sheet.update_acell("A1", 'Number of attempts')
